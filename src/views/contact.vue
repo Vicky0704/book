8fffc29660c184e7ff3572f253b8b48a13935d26
Vicky <vicky@ceis.com>
2022-09-20 17:01:25 +0800

    哈哈

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import { useStore } from '../stores/counter'
   import taiwan from './taiwan.vue'
   import place_data from '../assets/Swiper_demo.js'
   import { ref } from 'vue'
@@ -8,13 +9,16 @@
     .map(item => item.content)
   function clickmap(cm){
     console.log(cm);
-    // window.scrollTo(0,document.getElementById(cm).offsetTop);
     window.scrollTo({ 
       top: document.getElementById(cm).offsetTop, 
       behavior: "smooth" 
     });
     // location.hash='#'+cm;
   }
+  const store = useStore()
+  // function changeCount() {
+  //   store.count++;
+  // }
 </script>
 <template>
   <main>
@@ -26,19 +30,19 @@
           </ul>
         </div>
         <!-- InstanceEndEditable -->
-
         <!-- InstanceBeginEditable name="內容"main -->
         <div id="main">
           <h1 id="a01">聯絡我們</h1>
           <br>
-          <h2 id="aa5">台中分會</h2>
-          <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3640.72793302358!2d120.73026791541538!3d24.146190979457042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3469235d7fb2c4a7%3A0x1cc856130460088d!2z5ZyL56uL5Yuk55uK56eR5oqA5aSn5a24!5e0!3m2!1szh-TW!2stw!4v1635605080811!5m2!1szh-TW!2stw" width="425" height="350" style="border:0;" loading="lazy"></iframe></p>         
+          <!-- <button type="button" @click="changeCount">count is: {{ store.count }}, count2 is : {{store.count2}}</button> -->
+          <h2 id="aa5">{{store.main[0].name}}</h2>
+          <p><iframe src="{{store.main[0].src}}" width="425" height="350" style="border:0;" loading="lazy"></iframe></p>         
           <p>
             <ul class="tt01" style="list-style: none;">
-              <li>✈ 地&emsp;&emsp;址：臺中市太平區坪林里中山路二段57號</li>
-              <li>☏ 聯絡專線：(04)23924505</li>
-              <li>℻ 傳真電話：(04)23923363</li>
-              <li>✉ 信&emsp;&emsp;箱：<a style="border-right:none;" href="mailto:aaa123@gmail.com">aaa123@gmail.com</a></li>
+              <li>✈ 地&emsp;&emsp;址：{{store.main[0].add}}</li>
+              <li>☏ 聯絡專線：{{store.main[0].cphone}}</li>
+              <li>℻ 傳真電話：{{store.main[0].mphone}}</li>
+              <li>✉ 信&emsp;&emsp;箱：<a style="border-right:none;" href="mailto:{{store.main[0].mail}}">{{store.main[0].mail}}</a></li>
             </ul>
           </p>
           <br>
@@ -53,18 +57,21 @@
             <area href="#aa4" alt="屏東分校" shape="circle" coords="203,302,20">
             <area href="#aa5" alt="台中分校" shape="circle" coords="203,148,20">
           </map> -->
+
           <br>
-          <h2 id="taipei_city">台北分會</h2>
-          <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.6260057569243!2d121.53858061542581!3d25.012821145293152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442aa2176c4c0ad%3A0x90db5e44ee29f455!2z5ZyL56uL6Ie654Gj56eR5oqA5aSn5a24!5e0!3m2!1szh-TW!2stw!4v1635670672357!5m2!1szh-TW!2stw" width="425" height="350" style="border:0;" loading="lazy"></iframe></p>
-          <p>
-            <ul class="tt01" style="list-style: none;">
-              <li>✈ 地&emsp;&emsp;址：台北市大安區基隆路四段43號</li>
-              <li>☏ 聯絡專線：(02)27333141</li>
-              <li>✉ 信&emsp;&emsp;箱：<a style="border-right:none;" href="mailto:bbb123@gmail.com">bbb123@gmail.com</a></li>
-            </ul>
-          </p>
-          <br>[<a href="#a02">map</a>][<a href="#">top</a>]
-          <h2 id="yunlin_country">雲林分會</h2>
+          <template v-for="(item,index) of store.branch" :key="item.id">
+            <h2 id="taipei_city">{{item.name}}</h2>
+            <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.6260057569243!2d121.53858061542581!3d25.012821145293152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442aa2176c4c0ad%3A0x90db5e44ee29f455!2z5ZyL56uL6Ie654Gj56eR5oqA5aSn5a24!5e0!3m2!1szh-TW!2stw!4v1635670672357!5m2!1szh-TW!2stw" width="425" height="350" style="border:0;" loading="lazy"></iframe></p>
+            <p>
+              <ul class="tt01" style="list-style: none;">
+                <li>✈ 地&emsp;&emsp;址：{{item.add}}</li>
+                <li>☏ 聯絡專線：{{item.cphone}}</li>
+                <li>✉ 信&emsp;&emsp;箱：<a style="border-right:none;" href="mailto:bbb123@gmail.com">{{item.mail}}</a></li>
+              </ul>
+            </p>
+            <br>[<a href="#a02">map</a>][<a href="#">top</a>]
+          </template>
+          <!-- <h2 id="yunlin_country">雲林分會</h2>
           <p><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.439665260044!2d120.53187611541001!3d23.695988396768758!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346ec804c079615b%3A0xdf19d859983954be!2z5ZyL56uL6Zuy5p6X56eR5oqA5aSn5a24!5e0!3m2!1szh-TW!2stw!4v1635670709595!5m2!1szh-TW!2stw" width="425" height="350" style="border:0;" loading="lazy"></iframe></p>
           <p>
             <ul class="tt01" style="list-style: none;">
@@ -93,7 +100,7 @@
               <li>✉ 信&emsp;&emsp;箱：<a style="border-right:none; " href="mailto:eee123@gmail.com">eee123@gmail.com</a></li>
             </ul>
           </p>
-          <br>[<a href="#a02">map</a>][<a href="#">top</a>]
+          <br>[<a href="#a02">map</a>][<a href="#">top</a>] -->
           </div>
         <!-- InstanceEndEditable -->
   </main>
